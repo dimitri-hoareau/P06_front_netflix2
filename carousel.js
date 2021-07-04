b113eec1dd9dd8f51aaf0d7e185b4b0913a58066
dimitri-hoareau <dimitrihoareau2@gmail.com>
2021-07-04 19:17:47 +0200

    fix error in bullet point navigation

--- carousel.js
+++ carousel.js
@@ -1,6 +1,6 @@
 
     function navigate(id, index, type) {
-        let listElement = document.getElementsByClassName('carousel__navigation-item')
+        let listElement = document.getElementsByClassName('carousel__navigation-item' + type)
         for (let i = 0; i < listElement.length; i++) {
             if (listElement[i].getAttribute("data-active") === "active") {
                 if (listElement[i].getAttribute("id") === (id + type)) {
@@ -8,9 +8,9 @@
                 } else {
                     listElement[i].removeAttribute("data-active")
                     document.getElementById((id + type)).setAttribute("data-active", "active")
-                    for (let i = 0; i < document.getElementsByClassName('carousel__slide').length; i++) {
-                        if (document.getElementsByClassName('carousel__slide')[i].getAttribute("data-active") === "active") {
-                            document.getElementsByClassName('carousel__slide')[i].removeAttribute("data-active")
+                    for (let i = 0; i < document.getElementsByClassName('carousel__slide' + type).length; i++) {
+                        if (document.getElementsByClassName('carousel__slide' + type)[i].getAttribute("data-active") === "active") {
+                            document.getElementsByClassName('carousel__slide' + type)[i].removeAttribute("data-active")
                         }
                     }
                     document.getElementById('carousel__slide' + index + type).setAttribute("data-active", "active")
@@ -25,33 +25,56 @@
 
 
     function navigateNext(type) {
-        let listElement = document.getElementsByClassName('carousel__slide')
-
+        let listElement = document.getElementsByClassName('carousel__slide' + type)
 
         for (let i = 0; i < listElement.length; i++) {
-            if (listElement[i].getAttribute("data-active") === "active") {
-                listElement[i + 1].setAttribute("data-active", "active")
-                listElement[i].removeAttribute("data-active")
-                document.getElementById('carousel__navigation-item_' + i + type).removeAttribute("data-active")
-                document.getElementById('carousel__navigation-item_' + (i + 1) + type).setAttribute("data-active", "active")
-                window.location.href='#carousel__slide' + (i + 1) + type
+            if (listElement[i + 1]) {
+                console.log("here")
+                if (listElement[i].getAttribute("data-active") === "active") {
+                    listElement[i].removeAttribute("data-active")
+                    listElement[i + 1].setAttribute("data-active", "active")
+                    document.getElementById('carousel__navigation-item_' + i + type).removeAttribute("data-active")
+                    document.getElementById('carousel__navigation-item_' + (i + 1) + type).setAttribute("data-active", "active")
+                    window.location.href='#carousel__slide' + (i + 1) + type
+                    break;
+                }
+            } else {
+                console.log("here2")
+                if (listElement[i].getAttribute("data-active") === "active") {
+                    listElement[i].removeAttribute("data-active")
+                    listElement[0].setAttribute("data-active", "active")
+                    document.getElementById('carousel__navigation-item_' + i + type).removeAttribute("data-active")
+                    document.getElementById('carousel__navigation-item_' + "0" + type).setAttribute("data-active", "active")
+                    window.location.href='#carousel__slide' + "0" + type
+                    break;
+                }
 
-                break;
             }
         }
     }
 
     function navigatePrevious(type) {
-        let listElement = document.getElementsByClassName('carousel__slide')
+        let listElement = document.getElementsByClassName('carousel__slide' + type)
         for (let i = 0; i < listElement.length; i++) {
-            if (listElement[i].getAttribute("data-active") === "active") {
-                listElement[i - 1].setAttribute("data-active", "active")
-                listElement[i].removeAttribute("data-active")
-                document.getElementById('carousel__navigation-item_' + i + type).removeAttribute("data-active")
-                document.getElementById('carousel__navigation-item_' + (i - 1) + type).setAttribute("data-active", "active")
-                window.location.href='#carousel__slide' + (i - 1) + type
-                
-                break;
+            if (listElement[i - 1]) {
+                if (listElement[i].getAttribute("data-active") === "active") {
+                    listElement[i].removeAttribute("data-active")
+                    listElement[i - 1].setAttribute("data-active", "active")
+                    document.getElementById('carousel__navigation-item_' + i + type).removeAttribute("data-active")
+                    document.getElementById('carousel__navigation-item_' + (i - 1) + type).setAttribute("data-active", "active")
+                    window.location.href='#carousel__slide' + (i - 1) + type
+                    break;
+                }
+            } else {
+                arrayLenght = listElement.length - 1
+                if (listElement[i].getAttribute("data-active") === "active") {
+                    listElement[i].removeAttribute("data-active")
+                    listElement[arrayLenght].setAttribute("data-active", "active")
+                    document.getElementById('carousel__navigation-item_' + i + type).removeAttribute("data-active")
+                    document.getElementById('carousel__navigation-item_' + arrayLenght + type).setAttribute("data-active", "active")
+                    window.location.href='#carousel__slide' + "0" + type
+                    break;
             }
         } 
     }
+}
